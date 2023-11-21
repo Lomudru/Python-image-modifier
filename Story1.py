1d592dfccf3595672b3d7705e84f8bcea4b0892b
Lomudru <kmejasson@gmail.com>
2023-11-21 15:50:40 +0100

    Finished dilated image

--- Story1.py
+++ Story1.py
@@ -3,5 +3,5 @@ def BlackAndWhite(image,output):
     img=Image.open(image)
     filter = ImageEnhance.Color(img)
     img = filter.enhance(0)
-    img.show
+    img.show()
     img.save(f"{output}/BlackAndWhiteOf{image}")

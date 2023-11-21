648d65c389f0e0ea80596cf1ea13758739c322d7
Glouby <hoaraueloan0@gmail.com>
2023-11-21 16:23:07 +0100

    Story5

--- Story5.py
+++ Story5.py
@@ -1,15 +1,20 @@
 from PIL import Image
-def Resize(image,output):
+import cv2
+
+def Resize(image,output, largeur, longueur):
     """
     Convertie l'image mit en paramètre
     ex : >>> Blurred("../grimm.jpeg","img")
     renvoie une image floutée et la sauvegarde dans le dossier img"""
     img=Image.open(image)
-    img = img.resize((500, 256))
+    im = cv2.imread(image)
+    whidth = im.shape[1]
+    height = im.shape[0]
+    img = img.resize((round(whidth * largeur), round(height * longueur)))
     for i in range(len(image)):
         if image[i]=='/':
             NameImage=image[i+1:]
     NameImage = image
     img.show()
     img.save(f"{output}/ResizeOf{NameImage}")
-Resize("grimm.jpeg","img")
+Resize("grimm.jpeg","img", 0.5, 0.5)

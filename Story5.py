f6f26d9272d3d9a4cb44ed696c1511e9f0f7bad8
Glouby <hoaraueloan0@gmail.com>
2023-11-21 15:51:40 +0100

    Story5

--- Story5.py
+++ Story5.py
@@ -1,16 +1,15 @@
-from PIL import Image, ImageFilter
-def Blurred(image,output):
+from PIL import Image
+def Resize(image,output):
     """
     Convertie l'image mit en paramètre
     ex : >>> Blurred("../grimm.jpeg","img")
     renvoie une image floutée et la sauvegarde dans le dossier img"""
     img=Image.open(image)
-    img = img.filter(ImageFilter.BoxBlur(10))
-    NameImage = image
+    img = img.resize((500, 256))
     for i in range(len(image)):
         if image[i]=='/':
             NameImage=image[i+1:]
-    
+    NameImage = image
     img.show()
-    img.save(f"{output}/BlurredOf{NameImage}")
-Blurred("grimm.jpeg","img")
+    img.save(f"{output}/ResizeOf{NameImage}")
+Resize("grimm.jpeg","img")

2d747576aa19fcb7fe9d7fa16d90c5ee8c9cf04c
Alexis Godet <alexis.godet05@gmail.com>
2023-11-21 15:08:10 +0100

    Story1

---
+++ Story1.py
@@ -0,0 +1,7 @@
+from PIL import Image, ImageEnhance
+def BlackAndWhite(image,output):
+    img=Image.open(image)
+    filter = ImageEnhance.Color(img)
+    img = filter.enhance(0)
+    img.show
+    img.save(f"{output}/BlackAndWhiteOf{image}")

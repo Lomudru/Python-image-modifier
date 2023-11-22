172806c1057eae4264fc9324d2d164fbed362962
Lomudru <kmejasson@gmail.com>
2023-11-22 10:32:54 +0100

    Code de la story 1 et 3 Commenter

--- Story1.py
+++ Story1.py
@@ -1,13 +1,13 @@
 from PIL import Image, ImageEnhance
 def BlackAndWhite(image,output,save=1):
-    img=Image.open(image)
-    NameImage=image
-    for i in range(len(image)):
-        if image[i]=='/':
-            NameImage=image[i+1:]
-    filter = ImageEnhance.Color(img)
-    img = filter.enhance(0)
-    if save==1:
+    img=Image.open(image) # On Ouvre L'image 
+    NameImage=image # NameImage est le chemin de l'image
+    for i in range(len(image)): # Boucle qui parcours les charactère du chemin de l'image 
+        if image[i]=='/': # Si il y as un "/"
+            NameImage=image[i+1:] # On prend que ce qu'il y as après
+    filter = ImageEnhance.Color(img) # On applique la couleur noir et blanc a l'image
+    img = filter.enhance(0) 
+    if save==1: # Vérifie si l'orsque l'on a appeler la fonction l'utilisateur veut sauvegarder l'image
         img.save(f"{output}/BlackAndWhiteOf{NameImage}")
         return str(f"{output}/BlackAndWhiteOf{NameImage}")
     else:
--- Story3.py
+++ Story3.py
@@ -1,17 +1,14 @@
 import cv2 
 import numpy as np 
-
-def Dilatation(image,output,save=1):
-    img = cv2.imread(image)
-    NameImage = image
-    for i in range(len(image)):
-        if image[i]=='/':
-            NameImage=image[i+1:]
-    kernel = np.ones((10, 10), np.uint8)
-    img_dilate = cv2.dilate(img, kernel, iterations=1)
-    if save==1:
-        cv2.imwrite(f"{output}/Dilated{NameImage}", img_dilate)
-        return str(f"{output}/Dilated{NameImage}")
-    else:
-        cv2.imwrite(f"temps/Dilated{NameImage}", img_dilate)
+def Dilatation(image,output,save=1): img = cv2.imread(image) # Ouvre l'image 
+    NameImage = image # NameImage est le chemin de l'image 
+    for i in range(len(image)): # Boucle qui parcours les charactère du chemin de l'image 
+        if image[i]=='/': # Si il y as un "/" 
+            NameImage=image[i+1:] # On prend que ce qu'il y as après 
+    kernel = np.ones((10, 10), np.uint8) img_dilate = cv2.dilate(img, kernel, iterations=1) # Dilate l'image 
+    if save==1: # Vérifie si l'utilisateur veut sauvegarder 
+        cv2.imwrite(f"{output}/Dilated{NameImage}", img_dilate) 
+        return str(f"{output}/Dilated{NameImage}") 
+    else: 
+        cv2.imwrite(f"temps/Dilated{NameImage}", img_dilate) 
         return str(f"temps/Dilated{NameImage}")

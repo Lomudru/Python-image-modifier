64c389a252c333361a8a4f64728bb2b33ec80477
Glouby <hoaraueloan0@gmail.com>
2023-11-22 10:11:09 +0100

    Hop là un ptit com

--- Story2.py
+++ Story2.py
@@ -3,9 +3,13 @@ def Blurred(image,output):
     """
     Convertie l'image mit en paramètre
     ex : >>> Blurred("../grimm.jpeg","img")
-    renvoie une image floutée et la sauvegarde dans le dossier img"""
+    renvoie une image floutée et la sauvegarde dans le dossier img
+    """
+    # Sauvegarde l'image mit en paramètre dans la variable img
     img=Image.open(image)
+    # Applique le filtre
     img = img.filter(ImageFilter.BoxBlur(10))
+    # Vérifie si le chemin vers l'image contient des dossiers pour ne garder que le nom de l'image et son extension
     NameImage = image
     for i in range(len(image)):
         if image[i]=='/':

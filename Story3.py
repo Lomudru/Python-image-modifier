3556fc1d69c3de80f733a00c02ab1befa62f88dd
Lomudru <kmejasson@gmail.com>
2023-11-22 10:38:42 +0100

    Résolution d'un problème dans la Story3

--- Story3.py
+++ Story3.py
@@ -6,7 +6,8 @@ def Dilatation(image,output,save=1):
     for i in range(len(image)): # Boucle qui parcours les charactère du chemin de l'image 
         if image[i]=='/': # Si il y as un "/" 
             NameImage=image[i+1:] # On prend que ce qu'il y as après 
-    kernel = np.ones((10, 10), np.uint8) img_dilate = cv2.dilate(img, kernel, iterations=1) # Dilate l'image 
+    kernel = np.ones((10, 10), np.uint8) 
+    img_dilate = cv2.dilate(img, kernel, iterations=1) # Dilate l'image 
     if save==1: # Vérifie si l'utilisateur veut sauvegarder 
         cv2.imwrite(f"{output}/Dilated{NameImage}", img_dilate) 
         return str(f"{output}/Dilated{NameImage}") 

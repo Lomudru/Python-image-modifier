4f7978c9334b590cc92c2ad927e719b781f57087
Lomudru <kmejasson@gmail.com>
2023-11-22 15:51:51 +0100

    Problème résolut avec les log

--- Story6.py
+++ Story6.py
@@ -27,14 +27,14 @@ def WriteText(image, output, texte, position_x, position_y, size, save=1):
         log(f"Erreur de chemin de l'image {image} dans Write")
         return
     
-    imga = cv2.putText(images, texte, org=(position_x, position_y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=size, color=(255, 255, 255), thickness=3) # Ajoute le texte spécifié à l'image chargée aux coordonnées indiquées
-    log(f"Le texte {texte} a été mis sur l'image {NameImage} au coordonnée : {position_x} x, {position_y} y")
-
     NameImage = image # NameImage et le chemin de l'image
     for i in range(len(image)): # Parcourt chaque caractère de la chaîne représentant le chemin de l'image
         if image[i]=='/':
             NameImage=image[i+1:] # Si il y a un / on prend uniquement le chemin qui se trouve apres ce /
     
+    imga = cv2.putText(images, texte, org=(position_x, position_y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=size, color=(255, 255, 255), thickness=3) # Ajoute le texte spécifié à l'image chargée aux coordonnées indiquées
+    log(f"Le texte {texte} a été mis sur l'image {NameImage} au coordonnée : {position_x} x, {position_y} y")
+
     try:
         if save == 1: # Si on a choisit de sauvegarder ou qu'on a rien fait
             cv2.imwrite(f"{output}/WriteTextOn{NameImage}", imga) # Sauvegarde l'image dans le dossier output

45fbf722fed08e6ccad1a198eb05487f8ed36daf
Lomudru <kmejasson@gmail.com>
2023-11-24 09:39:41 +0100

    Ajout de commentaires pour la story 15

--- Story15.py
+++ Story15.py
@@ -3,7 +3,7 @@ from PIL import Image
 from logger import *
 
 def Make_gif(dossier, output):
-    
+    # Vérifie si l'information envoyer est bien un dossier
     if os.path.isdir(dossier):
         print("Vous devez mettre en paramètre le chemin d'un dossier (GIF)")
         log(f"Erreur d'extension du dossier {dossier} dans GIF")
@@ -11,16 +11,19 @@ def Make_gif(dossier, output):
 
 
     try:
+        # Liste les fichier du dossier
         Files = os.listdir(dossier)
+        # Les ouvre en tant qu'image
         Files = [Image.open(f"{dossier}/{file}") for file in sorted(Files)]
-        log("Les images sont trasformées en gif")
     except:
         print("Le chemin vers le dossier n'est pas bon ou n'existe pas. (GIF)")
         log(f"Erreur de chemin du dossier {dossier} dans GIF")
         return
     
     try:
+        # Crée le gif
         Files[0].save(f'{output}/Image.gif', save_all=True, append_images=Files[1:], optimize=False, duration=1000, loop=0)
+        log("Les images sont trasformées en gif")
     except:
         print("Le chemin vers la sauvegarde de votre GIF n'est pas bon ou n'existe pas. (GIF)")
         log(f"Erreur pour le chemin de sauvegarde du GIF dans la fonction GIF.")

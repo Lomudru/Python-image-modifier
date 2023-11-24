41696e8775302ff9744a7185176760db2da4f125
Lomudru <kmejasson@gmail.com>
2023-11-24 09:24:36 +0100

    --help réparer

--- main.py
+++ main.py
@@ -178,24 +178,25 @@ if '--i' and '--o' in args:
     except:
         print("Erreur dans les données donnée")
 
-    if '--help' in args:
-        print("Aide pour utiliser ce programme :")
-        print("Options disponibles :")
-        print("--i Pasth/Of/image.png: Chemin d'accès de l'image en entrée")
-        print("--o Pasth/Of/Output: Chemin d'accès de l'image de sortie")
-        print("--filters : Filtres disponibles (ex : \"WT:Texte,PosX,PosY,Size&Blur&Dila\")")
-        print("\"WT:Texte,PosX,PosY,Size\" ecrit du texte sur l'image")
-        print("\"BAW\" transforme en noir et blanc")
-        print("\"Blur\" Rend l'image flou")
-        print("\"Dila\" Dilate l'image")
-        print("\"Aqua\" Met un filtre aquarelle sur l'image")
-        print("\"Face\" Détecte les visages et met un rectangle autour")
-        print("\"Resize:longueur,largueur\" Redimentionne l'image")
-        print("\"Rotate:degre\" pivote l'image")
-        print("-log : Afficher le contenu du fichier 'movie.log'")
-        print("Exemple d'utilisation :")
-        print("python script.py --i chemin/vers/mon_image.jpg --o chemin/vers/sortie.jpg --filters \"WT:Hello,100,100,20&Blur\"")
-        print("Ceci appliquera un filtre de texte ('Hello') avec certaines coordonnées et flou à l'image en entrée.")
-        
+    
 else:
     print('Donner une image avec \'--i\'et un output avec \'--o\'')
+    
+if '--help' in args:
+    print("Aide pour utiliser ce programme :")
+    print("Options disponibles :")
+    print("--i Pasth/Of/image.png: Chemin d'accès de l'image en entrée")
+    print("--o Pasth/Of/Output: Chemin d'accès de l'image de sortie")
+    print("--filters : Filtres disponibles (ex : \"WT:Texte,PosX,PosY,Size&Blur&Dila\")")
+    print("\"WT:Texte,PosX,PosY,Size\" ecrit du texte sur l'image")
+    print("\"BAW\" transforme en noir et blanc")
+    print("\"Blur\" Rend l'image flou")
+    print("\"Dila\" Dilate l'image")
+    print("\"Aqua\" Met un filtre aquarelle sur l'image")
+    print("\"Face\" Détecte les visages et met un rectangle autour")
+    print("\"Resize:longueur,largueur\" Redimentionne l'image")
+    print("\"Rotate:degre\" pivote l'image")
+    print("-log : Afficher le contenu du fichier 'movie.log'")
+    print("Exemple d'utilisation :")
+    print("python script.py --i chemin/vers/mon_image.jpg --o chemin/vers/sortie.jpg --filters \"WT:Hello,100,100,20&Blur\"")
+    print("Ceci appliquera un filtre de texte ('Hello') avec certaines coordonnées et flou à l'image en entrée.")
